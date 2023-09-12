6884bc82fd7dbbb0cce3debabdf5e8b4a26df57b
peter michael <enitanpeters28@gmail.com>
2023-09-12 15:53:22 +0100

    authentication

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,17 +1,25 @@
 import { g, config, auth } from '@grafbase/sdk';
 
 // @ts-ignore
-const User = g.model('User', {
+const User = g
+ .model("User", {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  description: g
+   .string()
+   .length({ min: 2, max: 1000 })
+   .optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
-} ).auth( ( rules ) => {
-  rules.public().read()
-})
+  linkedinUrl: g.url().optional(),
+  projects: g
+   .relation(() => Project)
+   .list()
+   .optional(),
+ })
+ .auth((rules) => {
+  rules.public().read();
+ });
 
 // @ts-ignore
 const Project = g.model('Project', {
